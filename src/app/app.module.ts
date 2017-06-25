fd4ae2a3ee4f9d4e2c8af0df9160fe93a35a562c
nirkaufman <nirkaufman@gmail.com>
2017-06-25 12:37:47 +0300

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { UniqueDirective } from './user-form/unique.directive';
+import { SizeComponent } from './user-form/size/size.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserFormComponent,
+    UniqueDirective,
+    SizeComponent
   ],
   imports     : [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers   : [],
   bootstrap   : [AppComponent]
